94993fe142787bc0a061f7b23ed6c7e7d36b7d54
Kyle Zimmerman <kzdesignsios@gmail.com>
2024-04-08 20:35:07 -0700

    Initial Commit

---
+++ final_project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final_project
+//
+//  Created by Kyle Zimmerman on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
